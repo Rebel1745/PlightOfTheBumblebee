4555d24cb1903c77f6a70f81b48be2b021b241a9
Rebel1745 <31040719+Rebel1745@users.noreply.github.com>
2020-06-22 17:47:51 +0100

    Controller
    Another unsuccessful attempt

--- Plight of the Bumblebee/Assets/Scripts/BeeController.cs
+++ Plight of the Bumblebee/Assets/Scripts/BeeController.cs
@@ -4,30 +4,60 @@ using UnityEngine;
 
 public class BeeController : MonoBehaviour
 {
-    Rigidbody rb;
-
     void Start(){
-        rb = GetComponent<Rigidbody>();
+        Drone = GetComponent<Rigidbody>();
     }
 
-    public float MinRollRotation = -10f;
-    public float MaxRollRotation = 10f;
-    float currentRoll = 0f;
-    public float MinPitchRotation = -20f;
-    public float MaxPitchRotation = 20f;
-    float currentPitch = 0f;
+    public Rigidbody Drone;
 
-    public float MinSpeed = -2f;
-    public float MaxSpeed = 10f;
-    float currentSpeed = 0f;
+    /*Speed*/
+    public float ForwardBackward = 5f;
+    /*Speed*/
+    public float Tilt = 5f;
+    /*Speed*/
+    public float FlyLeftRight = 5f;
+    /*Speed*/
+    public float UpDown = 5f;
+
+    private Vector3 DroneRotation;
 
     void Update()
     {
-        UpdatePitchYawRoll();
-        UpdateSpeed();
+        DroneRotation = Drone.transform.localEulerAngles;
+        if (DroneRotation.z > 10 && DroneRotation.z <= 180) { Drone.AddRelativeTorque(0, 0, -10); }//if tilt too big(stabilizes drone on z-axis)
+        if (DroneRotation.z > 180 && DroneRotation.z <= 350) { Drone.AddRelativeTorque(0, 0, 10); }//if tilt too big(stabilizes drone on z-axis)
+        if (DroneRotation.z > 1 && DroneRotation.z <= 10) { Drone.AddRelativeTorque(0, 0, -3); }//if tilt not very big(stabilizes drone on z-axis)
+        if (DroneRotation.z > 350 && DroneRotation.z < 359) { Drone.AddRelativeTorque(0, 0, 3); }//if tilt not very big(stabilizes drone on z-axis)
+        
+        if (Input.GetKey(KeyCode.A)) { Drone.AddRelativeTorque(0, Tilt / -1, 0); }//tilt drone left
+        if (Input.GetKey(KeyCode.D)) { Drone.AddRelativeTorque(0, Tilt, 0); }//tilt drone right
+
+        if (DroneRotation.x > 10 && DroneRotation.x <= 180) { Drone.AddRelativeTorque(-10, 0, 0); }//if tilt too big(stabilizes drone on x-axis)
+        if (DroneRotation.x > 180 && DroneRotation.x <= 350) { Drone.AddRelativeTorque(10, 0, 0); }//if tilt too big(stabilizes drone on x-axis)
+        if (DroneRotation.x > 1 && DroneRotation.x <= 10) { Drone.AddRelativeTorque(-3, 0, 0); }//if tilt not very big(stabilizes drone on x-axis)
+        if (DroneRotation.x > 350 && DroneRotation.x < 359) { Drone.AddRelativeTorque(3, 0, 0); }//if tilt not very big(stabilizes drone on x-axis)
+
+
+        //Drone.AddForce(0, 9, 0);//drone not lose height very fast, if you want not to lose height al all change 9 into 9.80665 
+        
+        if (Input.GetKey(KeyCode.W)) { Drone.AddRelativeForce(0, 0, ForwardBackward); Drone.AddRelativeTorque(10, 0, 0); }//drone fly forward
+
+        if (Input.GetKey(KeyCode.A)) { Drone.AddRelativeForce(FlyLeftRight / -1, 0, 0); Drone.AddRelativeTorque(0, 0, 10); }//rotate drone left
+
+        if (Input.GetKey(KeyCode.D)) { Drone.AddRelativeForce(FlyLeftRight, 0, 0); Drone.AddRelativeTorque(0, 0, -10); }//rotate drone right
+
+        if (Input.GetKey(KeyCode.S)) { Drone.AddRelativeForce(0, 0, ForwardBackward / -1); Drone.AddRelativeTorque(-10, 0, 0); }// drone fly backward
+
+        if (Input.GetKey(KeyCode.Space)) { Drone.AddRelativeForce(0, UpDown, 0); }//drone fly up
+
+        if (Input.GetKey(KeyCode.LeftShift)) { Drone.AddRelativeForce(0, UpDown / -1, 0); }//drone fly down
+
+        //UpdatePitchYawRoll();
+        //UpdateSpeed();
+        //UpdateLookDirection();
     }
 
-    void UpdatePitchYawRoll()
+    /*void UpdatePitchYawRoll()
     {
         currentPitch = Mathf.Clamp(currentPitch += Input.GetAxis("Vertical"), MinPitchRotation, MaxPitchRotation);
         currentRoll = Mathf.Clamp(currentRoll += Input.GetAxis("Horizontal"), MinRollRotation, MaxRollRotation);
@@ -40,4 +70,9 @@ public class BeeController : MonoBehaviour
         currentSpeed = Mathf.Clamp(currentSpeed + Input.GetAxis("Vertical"), MinSpeed, MaxSpeed);
         rb.AddRelativeForce(Vector3.forward * currentSpeed);
     }
+
+    void UpdateLookDirection()
+    {
+        transform.LookAt(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+    }*/
 }
