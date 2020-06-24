8a2ddcd5ace9cb057c746b283f62ee8074725940
Rebel1745 <31040719+Rebel1745@users.noreply.github.com>
2020-06-24 16:02:36 +0100

    Bee Controller
    1st successful(ish) movement script.  Added a floor and some pillars to fly around

--- Plight of the Bumblebee/Assets/Scripts/BeeController.cs
+++ Plight of the Bumblebee/Assets/Scripts/BeeController.cs
@@ -5,59 +5,33 @@ using UnityEngine;
 public class BeeController : MonoBehaviour
 {
     void Start(){
-        Drone = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
     }
 
-    public Rigidbody Drone;
-
-    /*Speed*/
-    public float ForwardBackward = 5f;
-    /*Speed*/
-    public float Tilt = 5f;
-    /*Speed*/
-    public float FlyLeftRight = 5f;
-    /*Speed*/
-    public float UpDown = 5f;
-
-    private Vector3 DroneRotation;
+    Rigidbody rb;
 
     void Update()
     {
-        DroneRotation = Drone.transform.localEulerAngles;
-        if (DroneRotation.z > 10 && DroneRotation.z <= 180) { Drone.AddRelativeTorque(0, 0, -10); }//if tilt too big(stabilizes drone on z-axis)
-        if (DroneRotation.z > 180 && DroneRotation.z <= 350) { Drone.AddRelativeTorque(0, 0, 10); }//if tilt too big(stabilizes drone on z-axis)
-        if (DroneRotation.z > 1 && DroneRotation.z <= 10) { Drone.AddRelativeTorque(0, 0, -3); }//if tilt not very big(stabilizes drone on z-axis)
-        if (DroneRotation.z > 350 && DroneRotation.z < 359) { Drone.AddRelativeTorque(0, 0, 3); }//if tilt not very big(stabilizes drone on z-axis)
-        
-        if (Input.GetKey(KeyCode.A)) { Drone.AddRelativeTorque(0, Tilt / -1, 0); }//tilt drone left
-        if (Input.GetKey(KeyCode.D)) { Drone.AddRelativeTorque(0, Tilt, 0); }//tilt drone right
-
-        if (DroneRotation.x > 10 && DroneRotation.x <= 180) { Drone.AddRelativeTorque(-10, 0, 0); }//if tilt too big(stabilizes drone on x-axis)
-        if (DroneRotation.x > 180 && DroneRotation.x <= 350) { Drone.AddRelativeTorque(10, 0, 0); }//if tilt too big(stabilizes drone on x-axis)
-        if (DroneRotation.x > 1 && DroneRotation.x <= 10) { Drone.AddRelativeTorque(-3, 0, 0); }//if tilt not very big(stabilizes drone on x-axis)
-        if (DroneRotation.x > 350 && DroneRotation.x < 359) { Drone.AddRelativeTorque(3, 0, 0); }//if tilt not very big(stabilizes drone on x-axis)
-
-
-        //Drone.AddForce(0, 9, 0);//drone not lose height very fast, if you want not to lose height al all change 9 into 9.80665 
-        
-        if (Input.GetKey(KeyCode.W)) { Drone.AddRelativeForce(0, 0, ForwardBackward); Drone.AddRelativeTorque(10, 0, 0); }//drone fly forward
-
-        if (Input.GetKey(KeyCode.A)) { Drone.AddRelativeForce(FlyLeftRight / -1, 0, 0); Drone.AddRelativeTorque(0, 0, 10); }//rotate drone left
-
-        if (Input.GetKey(KeyCode.D)) { Drone.AddRelativeForce(FlyLeftRight, 0, 0); Drone.AddRelativeTorque(0, 0, -10); }//rotate drone right
-
-        if (Input.GetKey(KeyCode.S)) { Drone.AddRelativeForce(0, 0, ForwardBackward / -1); Drone.AddRelativeTorque(-10, 0, 0); }// drone fly backward
-
-        if (Input.GetKey(KeyCode.Space)) { Drone.AddRelativeForce(0, UpDown, 0); }//drone fly up
-
-        if (Input.GetKey(KeyCode.LeftShift)) { Drone.AddRelativeForce(0, UpDown / -1, 0); }//drone fly down
-
         //UpdatePitchYawRoll();
-        //UpdateSpeed();
-        //UpdateLookDirection();
+        UpdateDirectionMovement();
+        UpdateLookDirection();
+        UpdateCamera();
     }
 
-    /*void UpdatePitchYawRoll()
+    public float MinRollRotation = -10f;
+    public float MaxRollRotation = 10f;
+    float currentRoll = 0f;
+    public float MinPitchRotation = -20f;
+    public float MaxPitchRotation = 20f;
+    float currentPitch = 0f;
+
+    public float MinSpeed = -1f;
+    public float MaxSpeed = 1f;
+    public float LRSpeed = 1f;
+    public float UDSpeed = 1f;
+    float currentSpeed = 0f;
+
+    void UpdatePitchYawRoll()
     {
         currentPitch = Mathf.Clamp(currentPitch += Input.GetAxis("Vertical"), MinPitchRotation, MaxPitchRotation);
         currentRoll = Mathf.Clamp(currentRoll += Input.GetAxis("Horizontal"), MinRollRotation, MaxRollRotation);
@@ -65,14 +39,59 @@ public class BeeController : MonoBehaviour
         transform.rotation = Quaternion.Euler(currentPitch, 0.0f, -currentRoll);
     }
 
-    void UpdateSpeed()
+    void UpdateDirectionMovement()
     {
-        currentSpeed = Mathf.Clamp(currentSpeed + Input.GetAxis("Vertical"), MinSpeed, MaxSpeed);
-        rb.AddRelativeForce(Vector3.forward * currentSpeed);
+        if (Input.GetKey(KeyCode.W))
+        {
+            currentSpeed = MaxSpeed;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            currentSpeed = MinSpeed;
+        }
+        else
+        {
+            currentSpeed = 0f;
+        }
+
+        transform.Translate(Vector3.forward * currentSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * LRSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * LRSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(Vector3.up * UDSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.C))
+        {
+            transform.Translate(Vector3.down * UDSpeed * Time.deltaTime);
+        }        
     }
 
     void UpdateLookDirection()
     {
-        transform.LookAt(Camera.main.ScreenToWorldPoint(Input.mousePosition));
-    }*/
+        if (Input.GetAxis("Mouse X") != 0f)
+        {
+            transform.Rotate(0f, Input.GetAxis("Mouse X"), 0f);
+        }
+        if (Input.GetAxis("Mouse Y") != 0f)
+        {
+            transform.Rotate(Input.GetAxis("Mouse Y"), 0f, 0f);
+        }
+    }
+
+    void UpdateCamera()
+    {
+        Vector3 moveCamTo = transform.position - transform.forward * 10f + Vector3.up * 5f;
+        float bias = 0.96f;
+        Camera.main.transform.position = Camera.main.transform.position * bias + moveCamTo * (1f - bias);
+        Camera.main.transform.LookAt(transform.position + transform.forward * 30f);
+    }
 }
